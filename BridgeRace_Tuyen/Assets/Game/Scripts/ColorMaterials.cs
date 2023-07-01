c53b9980243e2a858669beefebf835326b346794
tuyennt090104 <Tuyen.NT224908@sis.hust.edu.vn>
2023-07-02 06:37:14 +0700

    after lesson 1

---
+++ BridgeRace_Tuyen/Assets/Game/Scripts/ColorMaterials.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Color{
+    Blue,
+    Brown,
+    Green,
+    Grey,
+    Pink,
+    Purple,
+    Red,
+    White,
+    Yellow
+}
+public class ColorMaterials : MonoBehaviour
+{
+    public static Material[] materials;
+    public static int numColor;
+    void Awake()
+    {
+        numColor = Enum.GetValues(typeof(Color)).Length;
+        materials = new Material[numColor];
+        int i = 0;
+        foreach (Color value in Enum.GetValues(typeof(Color)))
+        {
+            Debug.Log(value);
+            materials[i] = Resources.Load<Material>("Materials/" + value);
+            if (materials[i] == null)
+            {
+                Debug.Log(value + "   fail:" + "Materials/" + value);
+            }
+            ++i;
+        }
+    }
+    static public bool ChangColor(GameObject gameObject, Material material) {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        if(renderer == null)
+        {
+            return false;
+        }
+        renderer.material = material;
+        return true;
+    }
+}
---
+++ BridgeRace_Tuyen/Assets/Game/Scripts/test.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator ChangeColor()
+    {
+        Debug.Log(ColorMaterials.numColor);
+        for(int i = 0; i < ColorMaterials.numColor; ++i)
+        {
+            ColorMaterials.ChangColor(gameObject, ColorMaterials.materials[i]);
+            Debug.Log((Color)i);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    void Start()
+    {
+        //StartCoroutine(nameof(ChangeColor));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
